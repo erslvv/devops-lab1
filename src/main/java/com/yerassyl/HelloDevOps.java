6964c5d562887917060c6c168003651028c2e6ad
erslvv <erslvvll@gmail.com>
2025-09-21 22:09:57 +0500

    feat(devops-lab1): Hello with CI

--- src/main/java/com/yerassyl/HelloDevOps.java
+++ src/main/java/com/yerassyl/HelloDevOps.java
@@ -2,6 +2,6 @@ package com.yerassyl;
 
 public class HelloDevOps {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps");
+        System.out.println("Hello DevOps from Yerassyl 230103128");
     }
 }
